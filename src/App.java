bc3a1e85729f1a6f434524f0bba558ec67854476
Hustra03 <erik.paulinder@gmail.com>
2023-10-01 11:27:35 +0200

    modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -16,16 +16,6 @@ public class App {
     }
 
     public static void LinkedListQueueBinaryTreeTest() {
-        /*QueueLinkedListBinaryNode queue = new QueueLinkedListBinaryNode();
-        queue.add(new BinaryNode(1, 1));
-        queue.add(new BinaryNode(2, 2));
-
-        queue.add(new BinaryNode(3, 3));
-
-        queue.add(new BinaryNode(4, 4));
-
-        queue.printQueue(queue.head, 1);*/
-
         BinaryTree tree = new BinaryTree();
         for (int i = 1; i < 5; i++) {
             
